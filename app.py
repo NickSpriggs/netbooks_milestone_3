e447ccd7c5a4fb37e31a7e780f52728c95f3e985
NickSpriggs <ntspriggs@me.com>
2021-07-29 21:42:11 +0000

    Created landing page with basic structure and search bar

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, 
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+# This is the instance of our Flask app using pyMongo
+
 @app.route("/")
-def start():
-    return "hello"
+@app.route("/get_films")
+def get_films():
+    films = mongo.db.film_list.find()
+    return render_template("get_films.html", films=films)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
