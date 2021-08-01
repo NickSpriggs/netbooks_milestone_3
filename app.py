286fd6f2a1e89bdcd2833f849f1a654f15e973a1
NickSpriggs <ntspriggs@me.com>
2021-08-01 18:41:06 +0000

    Created login/registration functions and altered code to only allow registered users to add films/recommendations

--- app.py
+++ app.py
@@ -84,6 +84,68 @@ def add_rec():
     return get_film(request.form.get("title"))
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username from form already exists in database
+        existing_user = mongo.db.readflix_users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for('register'))
+        
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.readflix_users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Succesful")    
+        return redirect(url_for("profile", username=session["user"]))
+    return render_template("register.html")
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username input in login.html exists in db
+        existing_user = mongo.db.readflix_users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        if existing_user:
+        # if existing_user (the username) exists in database
+
+            # ensure hashed password matches user input
+            if check_password_hash(existing_user["password"], 
+            request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+                return get_films()
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # if existing_user, the username, doesn't exist
+            flash("Incorrect Username and/or Password")    
+            return redirect(url_for("login"))  
+
+    # else if method != POST just return login.html
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    # remove user from the session cookies
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+    
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
